7cc3776cefa4ebe41ac3b76d5db26d74b4866bbb
suusatoshigi <826787715@qq.com>
2015-12-10 00:30:53 +0800

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# return （返回）用 render 方法渲染模板 blog/post_list.html 而得到的结果.
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
