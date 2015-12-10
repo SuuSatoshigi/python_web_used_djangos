04bdadd74de8e7dc41689bf97e4bcfa43a4e85d0
suusatoshigi <826787715@qq.com>
2015-12-10 15:23:29 +0800

    动态

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # return （返回）用 render 方法渲染模板 blog/post_list.html 而得到的结果.
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
